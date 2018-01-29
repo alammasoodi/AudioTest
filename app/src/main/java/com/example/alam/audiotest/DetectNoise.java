61891fb474ef3391170b7e852f78eb655b7341df
alammasoodi <alam@trialx.com>
2018-01-29 15:03:53 +0530

    Audio test added

---
+++ app/src/main/java/com/example/alam/audiotest/DetectNoise.java
@@ -0,0 +1,68 @@
+package com.example.alam.audiotest;
+
+import android.media.MediaRecorder;
+
+import java.io.IOException;
+
+/**
+ * Created by alam on 29/1/18.
+ */
+
+public class DetectNoise  {
+    static final private double EMA_FILTER = 0.6;
+
+    private MediaRecorder mRecorder = null;
+    private double mEMA = 0.0;
+
+    public void start() {
+
+        if (mRecorder != null) {
+            mRecorder.release();
+        }
+
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile("/dev/null");
+
+            try {
+                mRecorder.prepare();
+                mRecorder.start();
+
+            } catch (IllegalStateException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            mEMA = 0.0;
+        }
+
+
+    public void stop() {
+        if (mRecorder != null) {
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+        }
+    }
+
+    public double getAmplitude() {
+        if (mRecorder != null)
+            //return  (mRecorder.getMaxAmplitude()/2700.0);
+            return   20 * Math.log10(mRecorder.getMaxAmplitude() / 2700.0);
+        else
+            return 0;
+
+    }
+
+    public double getAmplitudeEMA() {
+        double amp = getAmplitude();
+        mEMA = EMA_FILTER * amp + (1.0 - EMA_FILTER) * mEMA;
+        return mEMA;
+    }
+
+}
